e7c3265d33df917d8c66231f9ebf9c6d163f913c
singhrahul <singhrahul10041998@gmail.com>
2024-01-30 14:59:29 +0530

    Initial commit from Create Next App

---
+++ src/components/Acomplishments/Acomplishments.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+
+const data = [
+  { number: 30, text: 'Projects'},
+  { number: 30, text: 'tools and technologies', },
+  { number: 3000, text: 'LinkedIn Followers', },
+  { number: 30, text: 'Github Repositories', }
+];
+
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider/>
+  </Section>
+);
+
+export default Acomplishments;
