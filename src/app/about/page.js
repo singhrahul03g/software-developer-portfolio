8e0b88ebf7df840a77a1a2677894412dc542af01
singhrahul <singhrahul10041998@gmail.com>
2025-05-25 12:54:04 +0530

    changes applied

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -12,7 +12,7 @@ const AboutMe = () => {
       <div className="contact-section">
         <div >
           <h1>Rahul Singh</h1>
-          <p>I am a Software Engineer who is skilled in development of web application and mobile aplication. Also I am skilled 
+          <p>I am a Software Engineer who is skilled in development of web applications and mobile applications. Also I am skilled 
 in working with machine learning, deep learning & artificial intelligence. I have 4+ years of experience as a 
 software engineer. I worked for three years on mern stack developent and 1 years of experience on mobile 
 application development and AI engineer. My key skills are MERN stack, flutter & Machine learning and AI. I have 
